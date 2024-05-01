ea0c54f827b202eba23c1e36222929e5e42f5e39
Alexander Kalinin <arw.kalinin@gmail.com>
2024-05-01 16:55:30 +0300

    remote selenoid hub

--- src/test/java/demoqa/DemoqaBaseTest.java
+++ src/test/java/demoqa/DemoqaBaseTest.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 public class DemoqaBaseTest {
 
     static void beforeAllSuite() {
-        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "http://127.0.0.1:4444/wd/hub";
