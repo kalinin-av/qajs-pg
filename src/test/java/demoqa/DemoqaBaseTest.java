dad0ee2ecbcca94c9a5027e03419572697472a53
Alexander Kalinin <arw.kalinin@gmail.com>
2024-05-01 16:52:40 +0300

    remote selenoid hub

--- src/test/java/demoqa/DemoqaBaseTest.java
+++ src/test/java/demoqa/DemoqaBaseTest.java
@@ -14,7 +14,7 @@ public class DemoqaBaseTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.remote = "http://localhost:8080/wd/hub";
     }
 
     void beforeEachSuite() {
